5cc13070a3c40b2d5421ca99159fb4d6c4baa8aa
assidik <134124368+assidik12@users.noreply.github.com>
2023-10-31 05:37:39 +0700

    soal1.py

--- soal1.py
+++ soal1.py
@@ -5,4 +5,7 @@ matriks1 = np.array([[1,2,3], [4,5,6] , [7,8,9]])
 
 for x in range(0,len(matriks1)):
     for y in range(0,len(matriks1[0])):
+        #penjumlahan
         print(matriks1[x][y]+ matriks1[x][y])
+        #pengurangan
+        print(matriks1[x][y]- matriks1[x][y])
